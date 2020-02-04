4fe22f99a19948604e01b842c762d03e2ee63da9
arikim513 <arikim513@gmail.com>
2020-02-04 22:34:32 +0900

    자주 쓰는 미들웨어들

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //babel이 최신ES6문법을 변환해 줌
 import express from 'express';
+import morgan from 'morgan'; //logger
+import helmet from 'helmet'; //보안
+import cookieParcer from 'cookie-parser';
+import bodyParcer from 'body-parser';
+//옛날 문법
 // const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,28 +14,20 @@ const handleProfile = (req, res) => {
   res.send('You are on my profile with nodemon!');
 };
 
-const betweenMiddleware1 = (req, res, next) => {
-  console.log("I'm between 1");
-  next();
-};
-
-const betweenMiddleware2 = (req, res, next) => {
-  console.log("I'm between 2");
-  next();
-};
-
-const betweenMiddlewareAll1 = (req, res, next) => {
-  console.log("I'm between everywhere 1");
-  next();
-};
-
-const betweenMiddlewareAll2 = (req, res, next) => {
-  console.log("I'm between everywhere 2");
-  next();
-};
+//cookie
+app.use(cookieParcer());
+//form을 다룸.
+//우리 서버에게 유저가 보낼 데이터 종류를 알려줌.
+//서버가 이해해야 처리하니까.
+//보통의 form이라면 옵션으로 .urlencoded
+//json이라면 .json 등등
+app.use(bodyParcer.urlencoded({ extended: true }));
+app.use(bodyParcer.json());
+//helmet은 보안
+app.use(helmet());
+//morgan은 로깅 미들웨어
+app.use(morgan('dev'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-app.use(betweenMiddlewareAll1); //미들웨어 use한 순서대로 적용! 루팅할 곳 이전에 써 줄것!
-app.use(betweenMiddlewareAll2);
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/profile', betweenMiddleware1, betweenMiddleware2, handleProfile);
+app.get('/profile', handleProfile);
