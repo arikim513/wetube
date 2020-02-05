73b91d1418c4a17b9e2657690efdeba6c3314967
arikim513 <arikim513@gmail.com>
2020-02-05 22:38:27 +0900

    지역변수 사용법. render() 함수의 두번쨰 인자에 객체로 값을 넘길 수 있다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,8 @@
-export const home = (req, res) => res.render('home');
-export const search = (req, res) => res.render('search');
-export const videos = (req, res) => res.render('videos');
-export const upload = (req, res) => res.render('upload');
-export const videoDetail = (req, res) => res.render('videoDetail');
-export const editVideo = (req, res) => res.render('editVideo');
-export const deleteVideo = (req, res) => res.render('deleteVideo');
+//렌더함수 인자 중 첫번째는 템플릿이고, 두번째는 템플릿에 추가할 정보가 담긴 객체.
+export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
+export const search = (req, res) => res.render('search', { pageTitle: 'Search' });
+export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
+export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
+export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
+export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
+export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
