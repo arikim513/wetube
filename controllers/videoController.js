9533b5f4e9a7028c6d510013c0e4ee11578c35b5
arikim513 <arikim513@gmail.com>
2020-02-18 21:20:20 +0900

    Search controller(form에서 get으로 보낸 파라미터 화면에서 받아 표시)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,11 @@
 //렌더함수 인자 중 첫번째는 템플릿이고, 두번째는 템플릿에 추가할 정보가 담긴 객체.
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) => res.render('search', { pageTitle: 'Search' });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render('search', { pageTitle: 'Search', searchingBy });
+};
 export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
 export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
